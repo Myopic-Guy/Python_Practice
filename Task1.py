25fcca4cc303e6879b7c119e5e57056f83bea492
Kunal <kunalsharmadelhi932@gmail.com>
2025-10-15 20:31:18 +0530

    Task 1

---
+++ Task1.py
@@ -0,0 +1,13 @@
+#Task 1
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print(f"The sum of {a} and {b} is {a+b}")
+print(f"The subtraction of {a} and {b} is {a-b}")
+print(f"The multiplication of {a} and {b} is {a*b}")
+    
+
+
+#Task2
+First_Name=input("Enter your First Name: ")
+Last_Name=input("Enter your Last Name: ")
+print(f"Hello! {First_Name} {Last_Name} Welcome to Python Programming")
